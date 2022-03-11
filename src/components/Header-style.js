9b37ee329142d1c4cd78460166705d047e77d161
Jorge Matias <jorgematias.dev@gmail.com>
2022-03-11 20:16:44 +0000

    TDC-16 #comment Logo added

--- src/components/Header-style.js
+++ src/components/Header-style.js
@@ -1,8 +1,11 @@
 import styled from "styled-components"
+import LogoImage from "../logo.png"
 
-const Logo = styled.div`
-    background:red;
-    width:auto;
-    height:auto;
+const Logo = styled.img`
+    background-image: url(${LogoImage});
+    background-size: 100% 100%;
+    width:150px;
+    height:50px;
+    border:none;
     `
 export {Logo}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,6 @@ import { Logo } from "./Header-style";
 const Header = () => {
   return (
   <Logo />
-      
-
     )
 };
 
