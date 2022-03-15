eacd99c16a7318533b389c5cca25e54a962af059
Jorge Matias <jorgematias.dev@gmail.com>
2022-03-15 16:49:33 +0000

    TDC-32 #comment added z-a and byDate sorting

--- src/components/List.js
+++ src/components/List.js
@@ -24,8 +24,14 @@ const List = () => {
 };
 
 const Sorted = ({ sorted }) => {
-  if (sorted) return <div title="Sort alphabetically">Tasks</div>;
-  else return <div title="Sort by id">Tasks ↓</div>;
+  switch(sorted){
+    case 0:
+      return <div title="Sort alphabetically A-Z">Tasks</div>;
+    case 1:
+      return <div title="Sort alphabetically Z-A">Tasks ↓</div>;
+    case 2:
+      return <div title="Sort by id">Tasks ↑</div>;
+  }
 };
 
 export default List;
